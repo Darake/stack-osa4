fe0f122de103c641a7b5326b59970d822b723df0
Gert <adamson.gert@hotmail.com>
2019-02-17 17:08:47 +0200

    Tehtävä 4.8

---
+++ blogilista/tests/blog_api.test.js
@@ -0,0 +1,32 @@
+const supertest = require('supertest')
+const mongoose = require('mongoose')
+const helper = require('./test_helper')
+const app = require('../app')
+const api = supertest(app)
+
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+  await Blog.remove({})
+
+  for (let blog of helper.initialBlogs) {
+    let blogObject = new Blog(blog)
+    await blogObject.save()
+  }
+})
+
+test('right amount of blogs are returned', async () => {
+  const res = await api.get('/api/blogs')
+  expect(res.body.length).toBe(helper.initialBlogs.length)
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
