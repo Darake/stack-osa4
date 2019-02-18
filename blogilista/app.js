afb32d412a3ca1b168a1d7dd6a7ddb0adf39f0ed
Gert <adamson.gert@hotmail.com>
2019-02-18 10:55:05 +0200

    Tehtävä 4.15

--- blogilista/app.js
+++ blogilista/app.js
@@ -5,11 +5,13 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
 
 app.use(cors())
 app.use(bodyParser.json())
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 module.exports = app
---
+++ blogilista/tests/user_api.test.js
@@ -0,0 +1,36 @@
+const supertest = require('supertest')
+const mongoose = require('mongoose')
+const helper = require('./test_helper')
+const app = require('../app')
+const api = supertest(app)
+const User = require('../models/user')
+
+describe('when there is initially one user at db', async () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+    const user = User({ username: 'admin', password: 'admin' })
+    await user.save()
+  })
+
+  test('creation succeeds with a new username', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'daraku',
+      name: 'Gert Adamson',
+      password: 'salasana'
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd.length).toBe(usersAtStart.length + 1)
+
+    const usernames = usersAtEnd.map(u => u.username)
+    expect(usernames).toContain(newUser.username)
+  })
+})
